ce629b06b01d36d2c0f25b5f96cce24976709b0d
samuel1997 <samuelsilva1997@hotmail.com>
2019-02-15 16:00:52 -0200

    Adicionando id do usuário no construtor

--- src/PsyVtiger.php
+++ src/PsyVtiger.php
@@ -6,10 +6,10 @@ namespace VtigerWS;
 class PsyVtiger extends VtigerLogin
 {
 
-    public function __construct(string $urlBase, string $usuario, string $key)
+    public function __construct(string $urlBase, string $usuario, string $key, string $idUsuario = "")
     {
 
-        parent::__construct($urlBase, $usuario, $key);
+        parent::__construct($urlBase, $usuario, $key, $idUsuario);
     }
 
     public function getAccountByID($id)
@@ -26,8 +26,12 @@ class PsyVtiger extends VtigerLogin
         return $data->result[0];
     }
 
-    public function createAccount($objectJson, $moduleName)
+    public function createAccount(array $array, string $moduleName)
     {
+        $array['assigned_user_id'] = $this->userId;
+
+        $objectJson = json_encode($array);
+
         $params = array("sessionName" => $this->tokenSession, "operation" => 'create', "element" => $objectJson, "elementType" => $moduleName);
         $url = $this->urlBase . '/webservice.php';
 
